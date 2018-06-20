14fc65e893e18d6194cbf9bd30420a6cf778d4b7
rloza08 <rloza08@safeway.com>
2018-06-20 10:57:26 +0800

    Fixed indention 2

--- portinv/arpinvshow.py
+++ portinv/arpinvshow.py
@@ -9,24 +9,24 @@ from portinv.common.tmplparser import PageGenerator
 
 
 class ArpInvShow:
-    def __init__(self):
-        self.db = DbConnect('pidb')
-        self.db.connect()
+        def __init__(self):
+                self.db = DbConnect('pidb')
+                self.db.connect()
 
-    def getStoreSubnet(self):
-        self.StoreSubnets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-        return self.StoreSubnets
+        def getStoreSubnet(self):
+                self.StoreSubnets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+                return self.StoreSubnets
 
-    def mainPage(self, formField):
-        siteGroupValues = []
-        siteGroupValues.append([self.getStoreSubnet()])
-        restmpl = None
-        resPage = None
-        beg = None
-        end = None
-        params = {'beg': '', 'end': ''}
+        def mainPage(self, formField):
+                siteGroupValues = []
+                siteGroupValues.append([self.getStoreSubnet()])
+                restmpl = None
+                resPage = None
+                beg = None
+                end = None
+                params = {'beg': '', 'end': ''}
 
-        genObj = PageGenerator('arpinvshow.tmpl')
-        html = genObj.pageGenerate(restmpl, siteGroupValues, resPage, params)
-        # html = genObj.pageGenerate('', siteGroupValues)
-        return html
+                genObj = PageGenerator('arpinvshow.tmpl')
+                html = genObj.pageGenerate(restmpl, siteGroupValues, resPage, params)
+                # html = genObj.pageGenerate('', siteGroupValues)
+                return html
